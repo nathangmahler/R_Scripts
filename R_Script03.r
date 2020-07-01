d158222d762f48ee22d9de3797b3a873785ddb8b
nathangmahler <nathan.mahler87@gmail.com>
2020-07-01 14:17:07 -0600

    commit

--- R_Script03.r
+++ R_Script03.r
@@ -1,5 +1,7 @@
-#import the data set from excel using the readxl package. I called it GoldOil.
-#use t.test function to determine difference between 2 means
+#import the data set from excel using the readxl package. I called it GoldOil and Food_calories.
+#use t.test function to determine difference between 2 means, the first example is using an independent
+# drwn samples while the second is using paired samples, also the null hypothese are different
+
 t.test(GoldOil$Gold,GoldOil$Oil,alternative = "two.sided",mu=0,paired = FALSE,
        var.equal = FALSE,conf.level = 0.95)
 t.test(Food_Calories$Before,Food_Calories$After,alternative = "greater",mu=0,paired = TRUE,
